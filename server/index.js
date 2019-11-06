167169a8a575bb298cce3c9c7919f2ef3c52293e
Zack Miller <hotbodyfitness@gmail.com>
2019-11-05 19:14:44 -0800

    Updates for deployment

--- server/index.js
+++ server/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const compression = require('compression');
 // const axios = require('axios');
+const cors = require('cors');
 const port = 3006;
 
 const app = express();
 
-app.use('/', express.static('public'));
+app.use('/', express.static(__dirname + '/../'));
 app.use('/:gameId', express.static('public'));
 // app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
 
 // write code to handle getting data
 // let overview = 'http://ec2-13-57-13-174.us-west-1.compute.amazonaws.com'; // Josh
